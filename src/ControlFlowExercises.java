bf3780fab7c4a1451795354f120a3ea6d2a77970
T-Asia Fries <tasia.s.fries@gmail.com>
2023-12-13 09:22:29 -0600

    commented out code

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class ControlFlowExercises {
-    public static void main(String[] args) {
+//public class ControlFlowExercises {
+//    public static void main(String[] args) {
 //        FIRST EXERCISE
 //        (a)
 //        int i = 5;
