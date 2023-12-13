8bf88caa5e3caf5eac614e8314b6e6be0abd1e0d
T-Asia Fries <tasia.s.fries@gmail.com>
2023-12-13 12:05:42 -0600

    String exercise walkthrough

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -3,20 +3,20 @@ public class StringExercise {
 
 //        STRING BASICS
 
-        String needs = "We don't need no education";
+        String needs = "We don't need no education\n" +
+                "We don't need no thought control";
 
         System.out.println(needs);
-        System.out.println(needs.replace("education", "thought control"));
 
         String check = "Check \"this\" out!, \"s inside of \"s!";
 
         System.out.println(check);
 
-        String windows = "In windows, the main drive is usually C:'";
-        System.out.println(windows.replace("'", "\\"));
+        String windows = "In windows, the main drive is usually C:\\";
+        System.out.println(windows);
 
-        String backlashes = "I can do backlashes ', double backlashes '', and the amazing triple backlash '''!";
-        System.out.println(backlashes.replace("'", "\\"));
+        String backlashes = "I can do backlashes \\, double backlashes \\\\, and the amazing triple backlash \\\\\\!";
+        System.out.println(backlashes);
 
     }
 }
