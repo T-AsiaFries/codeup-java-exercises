c962a3c9ed226ef2cf4963feb59c4b39b63de920
T-Asia Fries <tasia.s.fries@gmail.com>
2023-12-13 12:29:07 -0600

    bob exercise walkthrough

--- src/Bob.java
+++ src/Bob.java
@@ -18,3 +18,39 @@ public class Bob {
         }
     }
 }
+
+// WALKTHROUGH
+
+//public class Bob {
+//
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//        boolean confirm;
+//
+//        System.out.println("Would you like to initiate a conversation?");
+//        String initiate = sc.nextLine();
+//        do {
+//            if(initiate.equalsIgnoreCase("y")) {
+//                System.out.println("What would you like to ask Bob?");
+//                String input = sc.nextLine();
+//
+//                if (input.contains("?")) {
+//                    System.out.println("Sure");
+//                }else if(input.endsWith("!")) {
+//                    System.out.println("Whoa, chill out!");
+//                } else if(input.isEmpty()) {
+//                    System.out.println("Fine. Be that way!");
+//                }else {
+//                    System.out.println("Whatever.");
+//                }
+//            }else {
+//                break;
+//            }
+//            System.out.println("Continue [y/n]");
+//            confirm = sc.nextLine().equalsIgnoreCase("y");
+//
+//        }while(confirm);
+//        System.out.println("Good chat!");
+//    }
+//}
