a8aa0de5163ca5ec7e921cc3e6dd4ee37bd4acb7
T-Asia Fries <tasia.s.fries@gmail.com>
2023-12-13 09:21:14 -0600

    commented out code

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -30,11 +30,11 @@ public class ControlFlowExercises {
 //            System.out.println(counter);
 //            counter -= 5;
 //        } while (counter >= -10);
-         int i = 2;
-         do {
-             System.out.println(i);
-           i += i * i;
-         } while (i < 1000000);
+//         int i = 2;
+//         do {
+//             System.out.println(i);
+//           i += i * i;
+//         } while (i < 1000000);
 
 //         SECOND EXERCISE
 //        for (int i = 1; i <= 100; i++) {
@@ -51,44 +51,44 @@ public class ControlFlowExercises {
 //        }
 
 //        THIRD EXERCISE
-        Scanner scanner = new Scanner(System.in);
-        boolean confirm;
-        do {
-            System.out.println("What number would you like to go up to?");
-            int userInt = scanner.nextInt();
-            System.out.println("Here is your table!");
-            System.out.println("number | squared | cubed");
-            System.out.println("------ | ------- | -----");
-
-            for(int i = 1; i <= userInt; i++){
-                int square = i * i;
-                int cubed = i * i * i;
-                System.out.printf("%-7s|%-9s|%-6s\n", i, square, cubed);
-            }
-            System.out.println("Continue? [y/n]");
-            confirm = scanner.next().equalsIgnoreCase("y");
-        }while(confirm);
-        System.out.println("Bye!");
-
-//        FOURTH EXERCISE
-        do {
-            System.out.println("Enter a numerical grade from 0 to 100: ");
-            int userGrade = scanner.nextInt();
-
-            if(userGrade >= 88 && userGrade <= 100) {
-                System.out.println("A");
-            }else if(userGrade >= 80 && userGrade <= 87) {
-                System.out.println("B");
-            }else if(userGrade >= 67 && userGrade <= 79) {
-                System.out.println("C");
-            }else if(userGrade >= 60 && userGrade <= 66) {
-                System.out.println("D");
-            }else {
-                System.out.println("F");
-            }
-            System.out.println("Continue? [y/n]");
-            confirm = scanner.next().equalsIgnoreCase("y");
-        }while(confirm);
-        System.out.println("Alright, thanks!");
-    }
-}
+//        Scanner scanner = new Scanner(System.in);
+//        boolean confirm;
+//        do {
+//            System.out.println("What number would you like to go up to?");
+//            int userInt = scanner.nextInt();
+//            System.out.println("Here is your table!");
+//            System.out.println("number | squared | cubed");
+//            System.out.println("------ | ------- | -----");
+//
+//            for(int i = 1; i <= userInt; i++){
+//                int square = i * i;
+//                int cubed = i * i * i;
+//                System.out.printf("%-7s|%-9s|%-6s\n", i, square, cubed);
+//            }
+//            System.out.println("Continue? [y/n]");
+//            confirm = scanner.next().equalsIgnoreCase("y");
+//        }while(confirm);
+//        System.out.println("Bye!");
+//
+////        FOURTH EXERCISE
+//        do {
+//            System.out.println("Enter a numerical grade from 0 to 100: ");
+//            int userGrade = scanner.nextInt();
+//
+//            if(userGrade >= 88 && userGrade <= 100) {
+//                System.out.println("A");
+//            }else if(userGrade >= 80 && userGrade <= 87) {
+//                System.out.println("B");
+//            }else if(userGrade >= 67 && userGrade <= 79) {
+//                System.out.println("C");
+//            }else if(userGrade >= 60 && userGrade <= 66) {
+//                System.out.println("D");
+//            }else {
+//                System.out.println("F");
+//            }
+//            System.out.println("Continue? [y/n]");
+//            confirm = scanner.next().equalsIgnoreCase("y");
+//        }while(confirm);
+//        System.out.println("Alright, thanks!");
+//    }
+//}
